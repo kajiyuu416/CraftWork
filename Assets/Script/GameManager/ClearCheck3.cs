1240ba6cd3caa036ab78e1cc4304a21a136f5139
kajiyuu416 <kajyuu416@gmail.com>
2024-03-27 12:26:24 +0900

    12;26

--- Assets/Script/GameManager/ClearCheck3.cs
+++ Assets/Script/GameManager/ClearCheck3.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 public class ClearCheck3 : MonoBehaviour
 {
     private bool ClearFlag = false;
-    [SerializeField] GameObject teleportObj1;
-    [SerializeField] GameObject teleportObj2;
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if(collision.CompareTag("Item") && !ClearFlag)
         {
             ClearFlag = true;
-            teleportObj1.SetActive(true);
-            teleportObj2.SetActive(true);
             SoundManager SM = SoundManager.Instance;
             SM.SoundPause();
             SM.SettingPlaySE7();
